691c71f025a6b2ba0a2a22ecc0f83a88b65656a8
AdriGV <adrian.gimeno.vigo@gmail.com>
2019-11-05 22:23:05 +0100

    :tada: Git initial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,65 @@
+/* 
+ * Copyright 2019 AdrianGV  adrian.gimeno.alum@iescamp.es.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author AdrianGV adrian.gimeno.alum@iescamp.es
+ */
+public final class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+
+        //Dato
+        int numero = 0;
+
+        //Control
+        boolean control = true;
+
+        //Bucle
+        do {
+            try {
+
+                //Peticion de numero
+                System.out.print("Inserte un numerito entero ..: ");
+                numero = SCN.nextInt();
+
+                //Salida
+                control = false;
+
+            } catch (Exception e) {
+
+                System.out.println("Error de entrada");
+
+            } finally {
+                SCN.nextLine();
+            }
+
+        } while (control);
+        
+        //Mensaje
+        System.out.println("<--->");
+        System.out.printf("El numerito es ...: %d%n", numero);
+
+    }
+}
